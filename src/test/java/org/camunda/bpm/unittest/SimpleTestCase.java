73bfbb11154e9cf2bd7aa39ab9f4b6374e65fd51
Tassilo Weidner <3015690+tasso94@users.noreply.github.com>
2022-10-20 16:09:28 +0200

    reproduce camunda/camunda-bpm-platform/issues/2848

--- src/test/java/org/camunda/bpm/unittest/SimpleTestCase.java
+++ src/test/java/org/camunda/bpm/unittest/SimpleTestCase.java
@@ -16,23 +16,19 @@
  */
 package org.camunda.bpm.unittest;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.assertThat;
-import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.complete;
-import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.processInstanceQuery;
-import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.runtimeService;
-import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.task;
-
-import org.camunda.bpm.engine.runtime.ProcessInstance;
-import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.engine.history.HistoricActivityInstance;
 import org.camunda.bpm.engine.test.junit5.ProcessEngineExtension;
+import org.camunda.bpm.engine.test.mock.Mocks;
+import org.camunda.bpm.engine.variable.Variables;
+import org.camunda.bpm.model.bpmn.Bpmn;
+import org.camunda.bpm.model.bpmn.BpmnModelInstance;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
-/**
- * @author Daniel Meyer
- * @author Martin Schimak
- */
+import java.util.logging.Logger;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class SimpleTestCase {
 
   @RegisterExtension
@@ -40,21 +36,55 @@ public class SimpleTestCase {
     .build();
 
   @Test
-  @Deployment(resources = {"testProcess.bpmn"})
-  public void shouldExecuteProcess() {
-    // Given we create a new process instance
-    ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("testProcess");
-    // Then it should be active
-    assertThat(processInstance).isActive();
-    // And it should be the only instance
-    assertThat(processInstanceQuery().count()).isEqualTo(1);
-    // And there should exist just a single task within that process instance
-    assertThat(task(processInstance)).isNotNull();
+  public void shouldFail() {
+    // given
+    BpmnModelInstance process = Bpmn.createExecutableProcess("testProcess")
+        .startEvent()
+          .exclusiveGateway()
+            .condition("true", "${myBean.myMethod(execution.getVariable('foo'), null, 47, execution.getVariable('bar'), execution.getVariable('baz'))}")
+            .userTask("userTask")
+          .moveToLastGateway()
+            .condition("false", "${false}")
+            .endEvent()
+        .done();
 
-    // When we complete that task
-    complete(task(processInstance));
-    // Then the process instance should be ended
-    assertThat(processInstance).isEnded();
+    extension.getRepositoryService().createDeployment()
+        .addModelInstance("testProcess.bpmn", process)
+        .deploy()
+        .getId();
+
+    Mocks.register("myBean", new MyBean());
+
+    // when
+    extension.getRuntimeService().startProcessInstanceByKey("testProcess",
+        Variables.createVariables()
+            .putValue("foo", "a")
+            .putValue("bar", null)
+            .putValue("baz", "x"));
+
+    // then
+    HistoricActivityInstance userTask = extension.getHistoryService().createHistoricActivityInstanceQuery()
+        .activityId("userTask")
+        .singleResult();
+    assertThat(userTask).isNotNull();
+  }
+
+  static class MyBean {
+
+    public boolean myMethod(String v, String w, int x, String y, String z) {
+      Logger.getLogger(this.getClass().getName())
+          .info(v + w + x + y + z);
+      return true;
+    }
+
+    public boolean myMethod(String v, String w, int x, String y) {
+      return myMethod(v, w, x, y, null);
+    }
+
+    public boolean myMethod(String v, String w, int x) {
+      return myMethod(v, w, x, null, null);
+    }
+
   }
 
 }
